43515efde0264d78b4c7e053efcd8b8c9f0976d6
Jeloisa <gimaltdinow-ruslan@yandex.ru>
2022-02-22 15:37:16 +0400

    Homework_lesson_1

---
+++ Lesson_1/ex_1.py
@@ -0,0 +1,3 @@
+user_name = input("Введите ваше имя: " )
+user_age = input("Введите ваш возраст: ")
+print('Привет, ', user_name)
---
+++ Lesson_1/ex_2.py
@@ -0,0 +1,5 @@
+time = int(input("Введите время в секундах "))
+hours = time // 3600
+minutes = (time - hours * 3600) // 60
+seconds = time - (hours * 3600 + minutes * 60)
+print(f"Время в формате чч:мм:сс   {hours:02d} : {minutes:02d} : {seconds:02d}")
